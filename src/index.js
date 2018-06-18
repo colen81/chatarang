d73ee47aa2e121aaa9783e86b37059a82702521d
Steve Norton <colen81@gmail.com>
2018-06-18 15:36:22 -0400

    Add url to rooms

--- src/index.js
+++ src/index.js
@@ -4,5 +4,11 @@ import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+
+ReactDOM.render(
+<Router>
+   <Route component = {App} />
+ </Router>   , 
+    document.getElementById('root'))
 registerServiceWorker()
